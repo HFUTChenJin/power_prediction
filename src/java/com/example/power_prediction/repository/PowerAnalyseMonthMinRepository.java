960d118b7881f4605a6160551a8ecf1bfe097ff8
HFUTChenJin <113986005+HFUTChenJin@users.noreply.github.com>
2024-08-22 18:25:09 +0800

    Delete src/java/com/example/power_prediction directory

--- src/java/com/example/power_prediction/repository/PowerAnalyseMonthMinRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.power_prediction.repository;
-
-import com.example.power_prediction.entity.PowerAnalyseMonthMin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PowerAnalyseMonthMinRepository extends JpaRepository<PowerAnalyseMonthMin,Integer> {
-    PowerAnalyseMonthMin findByDeviceIdAndDataTime(int deviceId, Integer dataTime);
-}
--- src/java/com/example/power_prediction/repository/ShiftSystemElectricityAssessmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.power_prediction.repository;
-
-
-import com.example.power_prediction.entity.ShiftSystemElectricityAssessment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShiftSystemElectricityAssessmentRepository extends JpaRepository<ShiftSystemElectricityAssessment,Integer> {
-}
